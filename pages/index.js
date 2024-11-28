e33507e65dc6bfc3cee901a990509f790fb37afe
Jean Garcia <jean_clg@hotmail.com>
2024-11-28 19:48:45 -0300

    create server

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+function Home() {
+  return (
+    <div>
+      <h1>Cristiane</h1>
+      <p>Bem-vinda ao meu primeiro exercício do curso que estou fazendo!</p>
+      <p>
+        Meu desafio com esse exercício é colocar uma aplicação online com uma
+        mensagem para alguém importante, e adivinha quem eu escolhi? 😊
+      </p>
+      <p>
+        Obrigado por aparecer na minha vida e torná-la muito mais gostosa e
+        prazerosa. Tem sido incrível acordar e dormir todos os dias pensando em
+        você, e tudo que mais quero é poder retribuir tudo o que você faz por
+        mim da melhor forma possível. Estar sempre ao seu lado, te apoiando em
+        tudo o que você for fazer, e estar contigo em todos os momentos, sejam
+        eles bons ou ruins.
+      </p>
+      <p>
+        Você é incrível, e estou com saudades. <span className="heart">S2</span>
+      </p>
+    </div>
+  );
+}
+export default Home;
